e1a09e1048b872c43b9dc66978c9e7f1456e59dd
N3u7r0 <brian_sabatini1993@hotmail.com>
2022-06-22 23:14:59 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("sistema de turnos")
+
+for (let i = 1; i < 10; i++) {
+    let nombre = prompt("Bienvenido!, ingrese su nombre");
+    let apellido = prompt("ingrese su apellido");
+    alert("Gracias por elegirnos " + nombre + " " + apellido + ". " + "Su turno es el numero " + i)
+}
+
+console.log("solo doy 10 turnos xq no me gusta laburar, ja.")
