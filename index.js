33ea6e39fe566b3c3fe4b6d2ebac8605febb7560
N3u7r0 <brian_sabatini1993@hotmail.com>
2022-06-22 23:56:13 -0300

    segundo commit, bucle con break para sistema de turnos apellido clave sabatini

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 console.log("sistema de turnos")
 
-for (let i = 1; i < 10; i++) {
+for (let i = 1; i < 15; i++) {
     let nombre = prompt("Bienvenido!, ingrese su nombre");
-    let apellido = prompt("ingrese su apellido");
-    alert("Gracias por elegirnos " + nombre + " " + apellido + ". " + "Su turno es el numero " + i)
+    let apellido = prompt("Ingrese su apellido");
+    alert("Gracias por elegirnos " + nombre + " " + apellido + ". " + "Su turno es el numero " + i);
+    if (apellido === "sabatini") {
+        alert("Brayannnn, te dejamos una virrita en la heladera, aguantame que cortamos los turnos papá")
+        break;
+    }
 }
+alert("Ups!!!, nos quedamos sin turnos automaticos, Disculpe las molestias.");
 
-console.log("solo doy 10 turnos xq no me gusta laburar, ja.")
+console.log("Solo doy 15 turnos xq no me gusta laburar, ja.")
