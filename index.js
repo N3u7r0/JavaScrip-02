69bf223f21ee2bb68d629b71d2fd42f07b937af3
N3u7r0 <brian_sabatini1993@hotmail.com>
2022-06-23 00:37:29 -0300

    sist de turnos pulido con apellido clave -sabatini- y turno 3 con reserva

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 console.log("sistema de turnos")
 
-for (let i = 1; i < 15; i++) {
+for (let i = 1; i < 150; i++) {
     let nombre = prompt("Bienvenido!, ingrese su nombre");
     let apellido = prompt("Ingrese su apellido");
     alert("Gracias por elegirnos " + nombre + " " + apellido + ". " + "Su turno es el numero " + i);
     if (apellido === "sabatini") {
-        alert("Brayannnn, te dejamos una virrita en la heladera, aguantame que cortamos los turnos papá")
+        alert("Brayannnn, te dejamos una virrita en la heladera, aguantame que cortamos los turnos papá");
         break;
     }
+    if (i === 15) {
+
+        break;
+    }
+    if (i === 3) {
+        alert("El turno numero 3 ya tiene una reserva, mil disculpas.")
+        continue;
+
+    }
+    console.log("turno numero " + i + " otorgado a " + apellido + " " + nombre + ".")
 }
 alert("Ups!!!, nos quedamos sin turnos automaticos, Disculpe las molestias.");
 
